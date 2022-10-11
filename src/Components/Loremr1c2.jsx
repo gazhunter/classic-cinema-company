7e583101d8f021c65d644cd666b3c0fbb8cf9a85
AndySmithQA <andy.smtih@qa.com>
2022-10-11 12:43:22 +0100

    HomePgaeText

---
+++ src/Components/Loremr1c2.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Loremr1c2 = () => {
+    return(
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+             Faucibus in ornare quam viverra. Venenatis tellus in metus vulputate. 
+             Egestas dui id ornare arcu odio ut sem. Quam
+              viverra orci sagittis eu volutpat odio facilisis mauris. 
+             Nunc pulvinar sapien et ligula ullamcorper. 
+            Eros in cursus turpis massa tincidunt dui ut. Lobortis 
+            feugiat vivamus at augue eget arcu
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+             Faucibus in ornare quam viverra. Venenatis tellus in metus vulputate. 
+             Egestas dui id ornare arcu odio ut sem. Quam
+              viverra orci sagittis eu volutpat odio facilisis mauris. 
+             Nunc pulvinar sapien et ligula ullamcorper. 
+            Eros in cursus turpis massa tincidunt dui ut. Lobortis 
+            feugiat vivamus at augue eget arcu
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+             Faucibus in ornare quam viverra. Venenatis tellus in metus vulputate. 
+             Egestas dui id ornare arcu odio ut sem. Quam
+             
+            
+            </p>
+    )
+}
+
+export default Loremr1c2;
