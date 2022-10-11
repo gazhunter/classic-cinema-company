509850da714dd55aede9015dc2f3f3c8ef8a8ab7
Deepak Sahu <Deepak.Sahu@muellergroup.com>
2022-10-11 12:56:44 +0200

    updating footer with timing data

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import "./OpeningTimes";
+import OpeningTimes from "./OpeningTimes";
 
 const Footer = () => {
   return (
     <footer className="footer footer-content">
+      <OpeningTimes />
       <div id="copyright">
-        Copyright &copy; The Classic Cinema Company Ltd 2022. All rights
-        reserved.
+        Copyright &copy; The Classic Cinema Company Ltd 2022.
+        All rights reserved.
       </div>
     </footer>
   );
